43a2ca0c6cc1e8619fc94dcbb84e5b6adfe99eb3
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-20 16:49:54 -0400

    Se introdujo los links

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../css/Productos.css'
+import '../css/Productos.css';
+import {Link} from 'react-router-dom';
 
 const Producto = (props) => {
   const {imagen, nombre, precio, id} = props.infoProducto;
@@ -7,7 +8,7 @@ const Producto = (props) => {
     <li>
       <img src={`img/${imagen}.png`} alt = {nombre}/>
       <p>{nombre} <span> $ {precio}</span></p>
-      <a href="#">Mas informacion</a>
+      <Link to={`/producto/${id}`}>Mas informacion</Link>
     </li>
    );
 }
